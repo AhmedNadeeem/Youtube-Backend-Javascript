9c98f9261b199fc968b2b1f4fc0c2d895d82006a
Ahmed Nadeem Gondal <ag1858085@gmail.com>
2025-11-07 19:40:10 +0500

    user register route and controller added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,24 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import router from "./routes/user.routes.js";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN || "*",
     credentials: true,
-}));
-app.use(express.urlencoded( { extended: true, limit: "16kb" } ));
-app.use(express.json( { limit: "16kb" } ));
+  })
+);
+
+// Middlewares
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// Routes declaration
+app.use("/api/v1/users", router);
 
 export { app };
